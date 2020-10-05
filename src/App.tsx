b7f439e3d7b7175244eca1533837b004e61e53ba
basheim <51891555+basheim@users.noreply.github.com>
2020-10-04 22:27:51 -0700

    header page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button, Box, Grid, Nav } from 'grommet';
+import { Home, Notification, ChatOption } from 'grommet-icons';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid
+        rows={['xsmall', ['small', 'large']]}
+        columns={['1/3', '2/3']}
+        fill="horizontal"
+        gap="small"
+        areas={[
+          { name: 'header', start: [0, 0], end: [1, 0] },
+          { name: 'nav', start: [0, 1], end: [0, 1] },
+          { name: 'main', start: [1, 1], end: [1, 1] },
+        ]}
+      >
+        <Nav gridArea="header" direction="row-reverse" background="brand" pad="small">
+          <Button icon={<Home />} size="small" hoverIndicator />
+          <Button icon={<Notification />} hoverIndicator />
+          <Button icon={<ChatOption />} hoverIndicator />
+          <header style={{size:'left'}}>Brandon's Blog</header>
+        </Nav>
+        <Box gridArea="nav" background="light-5" />
+        <Box gridArea="main" background="light-2" />
+      </Grid>
     </div>
   );
 }
