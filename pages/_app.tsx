fdcf2931cda665b7730f1f2a967cf4a2b9b8a580
basheim <51891555+basheim@users.noreply.github.com>
2021-04-15 17:15:28 -0700

    fix(): more flicker issues

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from '../lib/theme';
 export default function App({ Component, pageProps }: any) {
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles && jssStyles.parentElement) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
   return (<React.Fragment>
-    <CssBaseline />
-    <Component {...pageProps} />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
   </React.Fragment>);
 }
