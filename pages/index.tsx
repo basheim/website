4fdecb4231a3083c197144ed4aec0da343c8663c
basheim <51891555+basheim@users.noreply.github.com>
2021-04-16 23:59:00 -0700

    feat(): main page changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,65 @@
 import Layout from '../components/base/layout';
 import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Link from 'next/link';
+
+const useStyles = makeStyles((theme: any) => ({
+  mainFeatured: {
+    position: 'relative',
+    backgroundColor: theme.palette.grey[800],
+    color: theme.palette.common.white,
+    marginBottom: theme.spacing(4),
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    cursor: 'pointer'
+  },
+  overlay: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    backgroundColor: 'rgba(0,0,0,.3)',
+  },
+  mainFeaturedContent: {
+    position: 'relative',
+    padding: theme.spacing(3),
+    [theme.breakpoints.up('md')]: {
+      padding: theme.spacing(6),
+      paddingRight: 0,
+    }
+  },
+}));
 
 export default function Home({ }: any) {
+  const classes = useStyles();
   return (
-    <Layout home identity={{title: 'Welcome'}}>
+    <Layout home identity={{ title: 'Welcome' }}>
+      <Paper className={classes.mainFeatured}>
+        <div className={classes.overlay} />
+        <Link href={`/blog`}>
+          <Grid container>
+            <Grid item md={6}>
+              <div className={classes.mainFeaturedContent}>
+                <Typography component="h1" variant="h3" color="inherit" gutterBottom>
+                  Blog
+                            </Typography>
+                <Typography variant="h5" color="inherit" paragraph>
+                  Articles on anything and everything
+                            </Typography>
+              </div>
+            </Grid>
+          </Grid>
+        </Link>
+      </Paper >
+        <Typography component="h2" variant="h5">
+          Not a lot going on here. Still building...
+                                </Typography>
     </Layout>
   )
 }
